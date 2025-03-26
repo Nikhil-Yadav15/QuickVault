8ed6c4e4a86f84449956444933cffaed0af5bed0
Nikhil Yadav <70308863+Nikhil-Yadav15@users.noreply.github.com>
2025-03-26 20:43:18 +0530

    Added linkedin footer.js

--- app/componets/footer.js
+++ app/componets/footer.js
@@ -2,22 +2,20 @@ import React from 'react';
 
 const Footer = () => {
   return (
-    // <footer className="bg-gray-900 text-white py-4 text-center mt-auto">
     <footer className="bg-gradient-to-t from-gray-900 to-black py-3 mt-auto">
         <p className="text-center text-gray-300 text-sm md:text-base">
-          Created by <span className="font-semibold text-blue-400">Nikhil Yadav | ECE&apos;28</span>
-        </p>
+        Created by{' '}
+        <a
+          href="https://www.linkedin.com/in/nikhil-yadav-593a98321?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="font-semibold text-blue-400 hover:underline cursor-pointer"
+        >
+          Nikhil Yadav | ECE&apos;28
+        </a>
+      </p>
     </footer>
   );
 };
 
 export default Footer;
-
-
-{/* <footer className="w-full bg-black/30 backdrop-blur-sm py-3 mt-auto">
-      <div className="container mx-auto px-4">
-        <p className="text-center text-gray-300 text-sm md:text-base">
-          Created by <span className="font-semibold text-blue-400">Nikhil Yadav | ECE&apos;28</span>
-        </p>
-      </div>
-    </footer> */}
