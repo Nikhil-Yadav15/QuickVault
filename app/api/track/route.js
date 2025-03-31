db8d59f9a95069e6ea625f937c3d57669f241fb1
Nikhil Yadav <70308863+Nikhil-Yadav15@users.noreply.github.com>
2025-03-31 12:06:15 +0530

    Updated geoip route.js

--- app/api/track/route.js
+++ app/api/track/route.js
@@ -1,29 +1,52 @@
 import { headers } from 'next/headers';
 import clientPromise from "@/lib/mongodb";
+import { geoIpLookup } from '@/lib/geoip';
+import { deviceDetector } from '@/lib/device';
+import { getISP } from '@/lib/isp';
 
 export async function POST(request) {
-  const headersList = await headers();
-  const ip = headersList.get('x-forwarded-for')?.split(',')[0] || headersList.get('x-real-ip') || 'Unknown';
-  const userAgent = headersList.get('user-agent') || 'Unknown';
-  const referer = headersList.get('referer') || 'Direct';
-  const client = await clientPromise;
-  const db = client.db(process.env.MONGODB_DB);
-  const collections = db.collection(process.env.MONGODB_COLLECTION_VISITS);
-
-  const timestamp = new Date();
+  try {
+    const headersList = await headers();
+    const url = new URL(request.url);
+    const timestamp = new Date();
   const istOffset = 5.5 * 60;
   const istTimestamp = new Date(timestamp.getTime() + istOffset * 60 * 1000);
+    const ip = headersList.get('x-forwarded-for')?.split(',')[0] || headersList.get('x-real-ip') || 'Unknown';
+    
+    console.log("this is the ip going: ", ip);
+    const geoData = await geoIpLookup(ip);
+    const isp = await getISP(ip);
+    const deviceData = deviceDetector(headersList.get('user-agent') || '');
 
-  await collections.insertOne({
-    ip,
-    userAgent,
-    referer,
-    page: '/',
-    timestamp: istTimestamp,
-  });
+    const visitData = {
+      ip,
+      isp,
+      ...geoData,
+      ...deviceData,
+      referer: headersList.get('referer') || 'Direct',
+      referrerDomain: headersList.get('referer') 
+        ? new URL(headersList.get('referer')).hostname 
+        : 'Direct',
+      page: url.pathname,
+      queryParams: Object.fromEntries(url.searchParams),
+      timestamp: istTimestamp,
+    };
+    const client = await clientPromise;
+    const db = client.db(process.env.MONGODB_DB);
+    await db.collection(process.env.MONGODB_COLLECTION_VISITS).insertOne(visitData);
 
-  return new Response(JSON.stringify({ success: true }), {
-    status: 200,
-    headers: { 'Content-Type': 'application/json' },
-  });
+    return new Response(JSON.stringify({ success: true }), {
+      status: 200,
+      headers: { 'Content-Type': 'application/json' },
+    });
+  } catch (error) {
+    console.error('user info error:', error);
+    return new Response(JSON.stringify({ 
+      success: false, 
+      error: error.message 
+    }), {
+      status: 500,
+      headers: { 'Content-Type': 'application/json' },
+    });
+  }
 }
