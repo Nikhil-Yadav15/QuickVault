a15b584e5161a7bac5949c45b93376060aa51e50
Nikhil Yadav <70308863+Nikhil-Yadav15@users.noreply.github.com>
2025-04-01 18:55:49 +0530

    added css file of popup popup.js

--- app/componets/popup.js
+++ app/componets/popup.js
@@ -1,7 +1,7 @@
 "use client";
 import React from "react";
 import Popup from "reactjs-popup";
-import "reactjs-popup/dist/index.css";
+import "@/app/forPopup.css";
 import { QrCode } from "lucide-react";
 import { QRCodeSVG } from "qrcode.react";
 
